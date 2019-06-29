86b01bf02c77aebb2192c91282fed83fa1871ca4
Farahani Samat <farahanis@gmail.com>
2019-06-29 23:16:57 +1000

    Refactor

--- toy_robot.py
+++ toy_robot.py
@@ -1,6 +1,10 @@
 dir_list = ["NORTH", "SOUTH", "EAST", "WEST"]
 
 
+class InputError(Exception):
+    pass
+
+
 class ToyRobot:
     def __init__(self, x=0, y=0, direction="NORTH"):
         self.x = x
@@ -9,38 +13,21 @@ class ToyRobot:
 
     def place(self, input_x, input_y, input_direction):
         if input_x in range(5) and input_y in range(5) and input_direction in dir_list:
-            try:
-                self.x = input_x
-                self.y = input_y
-                self.direction = input_direction
-            except NameError:
-                return
+            self.x = input_x
+            self.y = input_y
+            self.direction = input_direction
         else:
-            print("Invalid values")
+            raise InputError
 
     def move(self):
-        if self.direction == "NORTH":
-            if self.y < 4:
-                self.y += 1
-            else:
-                return self.y
-        elif self.direction == "SOUTH":
-            if self.y > 0:
-                self.y -= 1
-            else:
-                return self.y
-        elif self.direction == "EAST":
-            if self.x < 4:
-                self.x += 1
-            else:
-                return self.x
-        elif self.direction == "WEST":
-            if self.x > 0:
-                self.x -= 1
-            else:
-                return self.x
-        else:
-            return
+        if self.direction == "NORTH" and self.y < 4:
+            self.y += 1
+        elif self.direction == "SOUTH" and self.y > 0:
+            self.y -= 1
+        elif self.direction == "EAST" and self.x < 4:
+            self.x += 1
+        elif self.direction == "WEST" and self.x > 0:
+            self.x -= 1
 
     def left(self):
         if self.direction == "NORTH":
